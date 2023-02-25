54a7adbea4fc132e31b76c6d0f7e22702263027d
kenjopa <kenjopa6@gmail.com>
2023-02-25 23:16:28 +0500

    added localstorage

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,56 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted,watch } from 'vue';
 
 let id = 0;
 
 const newTodo = ref('');
 const todos = ref([
-  { id: id++, text: 'Learn HTML' },
-  { id: id++, text: 'Learn JavaScript' },
-  { id: id++, text: 'Learn Vue' }
 ]);
 
 function addTodo() {
-  todos.value.push({ id: id++, text: newTodo.value });
-  newTodo.value = '';
+  if (newTodo.value.trim() == '' || newTodo.value === null) return 
+  todos.value.push({ 
+    id: id++, 
+    text: newTodo.value, 
+    done: false 
+  });
+  newTodo.value = ''
 }
 
 function removeTodo(todo) {
   todos.value = todos.value.filter((el) => el !== todo);
 }
+
+
+watch(todos, (newValue) => {
+  localStorage.setItem('todos', JSON.stringify(newValue))
+}, {deep: true})
+
+onMounted(() => {
+	todos.value = JSON.parse(localStorage.getItem('todos')) || []
+})
 </script>
 
 <template>
-  <form @submit.prevent="addTodo">
-    <input v-model="newTodo" />
-    <button>Add Todo</button>
-  </form>
-  <ul>
-    <li v-for="todo in todos" :key="todo.id">
-      {{ todo.text }}
-      <button @click="removeTodo(todo)" type="checkbox">Delete</button>
-    </li>
-  </ul>
+  <div class="wrapper">
+    <div class="header">
+      <h1 class="header__title">Tasks</h1>
+    </div>
+    <form @submit.prevent="addTodo">
+      <h4>Add todo</h4>
+      <input type="text" v-model="newTodo" placeholder="e.g. Learn Vue" />
+      <button class="btn btn-add">🚀</button>
+    </form>
+    <ul>
+      <div class="list">
+        <li v-for="todo in todos" :key="todo.id" :class="`list__item ${todo.done}`">
+          <input type="checkbox" v-model="todo.done" class="list__checkbox">
+          <span class="list__text">{{ todo.text }}</span>
+          <button @click="removeTodo(todo)" type="checkbox" class="btn btn-delete" >✖</button>
+        </li>
+        <!-- <button @click="isChecked = !isChecked" class="btn">{{!isChecked ? 'Check all' : 'Uncheck All'}}</button> -->
+      </div>
+    </ul>
+    
+  </div>
 </template>
